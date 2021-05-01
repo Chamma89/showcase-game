dbd6a42d61cf2583f69dbf8eedcfc181d87d7079
Fouad Chamma <Fouad.chamma@99designs.com>
2021-05-01 15:12:54 +1000

    Use ApolloClient to fetch data from api and display in UI using apollo hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from "react";
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+interface Props {
+  name: string;
+  code: string;
+}
+
+function CountriesList() {
+  const { loading, error, data } = useQuery(LIST_COUNTRIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+
+  return data.countries.map(({ name, code }: Props) => (
+    <div key={name}>
+      <p>
+        {name}: {code}
+      </p>
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
+        <CountriesList />
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { render } from "react-dom";
+import { ApolloProvider } from "@apollo/client/react";
 
-ReactDOM.render(
-  <React.StrictMode>
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com",
+  cache: new InMemoryCache(),
+});
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
