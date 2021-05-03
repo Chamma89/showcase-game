4b2571fda00fa5882bb865858b91c79b883215b9
Fouad Chamma <Fouad.chamma@99designs.com>
2021-05-03 15:12:56 +1000

    Add mutation code for future use and add useEffect hook to check for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { LIST_COUNTRIES } from "./graphql/Queries";
 import CountryCard from "./components/CountryCard";
 
 function App() {
   const { loading, error, data } = useQuery(LIST_COUNTRIES);
+  // const [addTodo] = useMutation(ADD_COUNTRY);
+
+  useEffect(() => {
+    if (data) {
+      console.log("data loaded man");
+    }
+  }, [data]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -1,9 +1,10 @@
 import { gql } from "@apollo/client";
 
 export const LIST_COUNTRIES = gql`
-  {
+  query ListCountries {
     countries {
       name
+      capital
       code
       emoji
       languages {
@@ -12,3 +13,13 @@ export const LIST_COUNTRIES = gql`
     }
   }
 `;
+
+// export const ADD_COUNTRY = gql`
+//   mutation AddCountry($name: String!) {
+//     addCountry(name: $name) {
+//       countries {
+//         name
+//       }
+//     }
+//   }
+// `;
